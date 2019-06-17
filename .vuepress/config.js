17a4d9808d9ecde17ea3e81ad9b141e188026541
Supriya Srivatsa <sup0508@gmail.com>
2019-06-17 19:23:14 +0530

    Add GA

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    plugins: [
+        [ 
+          '@vuepress/google-analytics',
+          {
+            'ga': 'UA-142247486-1'
+          }
+        ]  
+    ],
     markdown: {
         anchor: { permalink: false },
     }
